8a2b4e90832a8c44207f10c5a0dd19c721d5fb8d
Samuel Matos <s.mota1989@hotmail.com>
2023-06-14 20:15:04 -0300

    ajustes nos endpoints

--- GoFast.API/Program.cs
+++ GoFast.API/Program.cs
@@ -12,6 +12,7 @@ using GoFast.API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Security.Claims;
 using System.Text;
 
@@ -50,9 +51,49 @@ builder.Services.AddAuthorization(options =>
 });
 
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen(c =>
 {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiGoFast", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = @"JWT Authorization header usando o schema Bearer
+                       \r\n\r\n Informe 'Bearer'[space].
+                        Exemplo: \'Bearer 12345abcdef\'",
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+
     c.ResolveConflictingActions(x => x.First());
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
 });
 
 var app = builder.Build();
@@ -63,31 +104,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors();
+
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
+
 #region Usuario
 
-//app.MapPost("/login", async (
-//    SqlContext context,
-//    Login model) =>
-//    await context.Usuario.Where(x => x.LoginUser == model.LoginUser && x.Senha == model.Senha).FirstAsync()
-//        is Usuario user 
-//        ? Results.Ok(new
-//            {
-//                usuario = user,
-//                token = TokenService.GenerateToken(user)
-//            })
-//        : Results.NotFound(new
-//        {
-//            message = "Usuário ou senha incorreto!"
-//        }))
-//    .WithName("Login")
-//    .WithTags("Usuario");
-
-app.MapPost("/v1/CreateUsuario", async (IUsuarioRepository usuarioRepository, IHashService hashService, UsuarioViewModel model) =>
+app.MapPost("/api/Usuario/Create", async (IUsuarioRepository usuarioRepository, IHashService hashService, UsuarioViewModel model) =>
 {
     if (await usuarioRepository.VerificaSeUsuarioExiste(model.LoginUser))
         return Results.Ok(new
@@ -113,9 +140,11 @@ app.MapPost("/v1/CreateUsuario", async (IUsuarioRepository usuarioRepository, IH
     {
         message = $"Usuario {usuario.Nome} cadastrado com sucesso!"
     });
-}).AllowAnonymous().WithName("CreateUsuario").WithTags("Usuario");
+})
+    .AllowAnonymous()
+    .WithTags("Usuario");
 
-app.MapPut("/v1/statusUsuario", async (IUsuarioRepository usuarioRepository, UsuarioStatusViewModel model) =>
+app.MapPut("/api/Usuario/UpdateStatusUsuario", async (IUsuarioRepository usuarioRepository, UsuarioStatusViewModel model) =>
 {
     var usuario = await usuarioRepository.GetUsuarioByLogin(model.LoginUser);
 
@@ -139,9 +168,11 @@ app.MapPut("/v1/statusUsuario", async (IUsuarioRepository usuarioRepository, Usu
         {
             message = "Usuário desbloqueado com sucesso!"
         });
-}).RequireAuthorization("Admin").WithName("UpdateUsuario").WithTags("Usuario");
+})
+    .RequireAuthorization("Admin")
+    .WithTags("Usuario");
 
-app.MapPost("/v1/login", async (IUsuarioRepository usuarioRepository, IHashService hashService, LoginViewModel model) =>
+app.MapPost("/api/Usuario/login", async (IUsuarioRepository usuarioRepository, IHashService hashService, LoginViewModel model) =>
 {
     if (string.IsNullOrEmpty(model.LoginUser) || string.IsNullOrEmpty(model.Senha))
         return Results.NotFound(new
@@ -180,13 +211,15 @@ app.MapPost("/v1/login", async (IUsuarioRepository usuarioRepository, IHashServi
         usuario = usuario,
         token = token
     });
-}).AllowAnonymous().WithName("Login").WithTags("Usuario");
+})
+    .AllowAnonymous()
+    .WithTags("Usuario");
 
 #endregion
 
 #region Blob Storage
 
-app.MapPost("/v1/uploadImage", async (IBlobStorageRepository blobStorageRepository, IBlobStorageService blobStorageService, UploadImageViewModel model, ClaimsPrincipal user) =>
+app.MapPost("/api/Imagem/Upload", async (IBlobStorageRepository blobStorageRepository, IBlobStorageService blobStorageService, UploadImageViewModel model, ClaimsPrincipal user) =>
 {
     if (string.IsNullOrEmpty(model.Imagem))
         return Results.BadRequest(new
@@ -243,9 +276,11 @@ app.MapPost("/v1/uploadImage", async (IBlobStorageRepository blobStorageReposito
         });
     }
 
-}).WithName("UploadImage").WithTags("Blob Storage"); //.RequireAuthorization("Admin"); .RequireAuthorization()
+})
+    .RequireAuthorization()
+    .WithTags("Imagem");
 
-app.MapPost("/v1/deleteImage", async (IBlobStorageRepository blobStorageRepository, IBlobStorageService blobStorageService, DeleteImageViewModel model, ClaimsPrincipal user) =>
+app.MapPost("/api/Imaggem/Delete", async (IBlobStorageRepository blobStorageRepository, IBlobStorageService blobStorageService, DeleteImageViewModel model, ClaimsPrincipal user) =>
 {
     if (model.IdBlob == null)
         return Results.BadRequest(new
@@ -275,9 +310,11 @@ app.MapPost("/v1/deleteImage", async (IBlobStorageRepository blobStorageReposito
     {
         message = "Arquivo removido com sucesso!"
     });
-}).RequireAuthorization().WithName("DeleteImage").WithTags("Blob Storage"); //.RequireAuthorization("Admin");
+})
+    .RequireAuthorization()
+    .WithTags("Imagem");
 
-app.MapGet("/v1/GetById", async (IBlobStorageRepository blobStorageRepository, IMapper _mapper, Guid idBlobStorage) =>
+app.MapGet("/api/Imagem/GetById", async (IBlobStorageRepository blobStorageRepository, IMapper _mapper, Guid idBlobStorage) =>
 {
     if (string.IsNullOrEmpty(idBlobStorage.ToString()))
         return Results.BadRequest(new
@@ -291,13 +328,16 @@ app.MapGet("/v1/GetById", async (IBlobStorageRepository blobStorageRepository, I
         return Results.Ok(_mapper.Map<BlobStorageViewModel>(blobStorage));
 
     return Results.NoContent();
-}).WithName("GetImageById").WithTags("Blob Storage");
+})
+    .RequireAuthorization()
+    .WithName("GetImageById")
+    .WithTags("Imagem");
 
 #endregion
 
 #region Motorista
 
-app.MapGet("/v1/GetAll", async (IMotoristaRepository motoristaRepository, IMapper _mapper) =>
+app.MapGet("/api/Motorista/GetAll", async (IMotoristaRepository motoristaRepository, IMapper _mapper) =>
  {
      var motoristaDM = await motoristaRepository.GetAllMotoristas();
 
@@ -305,9 +345,11 @@ app.MapGet("/v1/GetAll", async (IMotoristaRepository motoristaRepository, IMappe
         return Results.Ok(_mapper.Map<List<MotoristaViewModel>>(motoristaDM));
 
     return Results.NoContent();
-}).WithName("GetAllMotoristas").WithTags("Motorista");
+})
+    .RequireAuthorization()
+    .WithTags("Motorista");
 
-app.MapGet("/v1/GetMotoristaById", async (IMotoristaRepository motoristaRepository, IMapper _mapper, Guid idMotorista) =>
+app.MapGet("/api/Motorista/GetById", async (IMotoristaRepository motoristaRepository, IMapper _mapper, Guid idMotorista) =>
 {
     var motoristaDM = await motoristaRepository.GetMotoristaById(idMotorista);
 
@@ -315,9 +357,11 @@ app.MapGet("/v1/GetMotoristaById", async (IMotoristaRepository motoristaReposito
         return Results.Ok(_mapper.Map<MotoristaViewModel>(motoristaDM));
 
     return Results.NoContent();
-}).WithName("GetMotoristaById").WithTags("Motorista");
+})
+    .RequireAuthorization()
+    .WithTags("Motorista");
 
-app.MapPost("/v1/Create", async (IMotoristaRepository motoristaRepository, IMapper _mapper, MotoristaInputModel model) =>
+app.MapPost("/api/Motorista/Create", async (IMotoristaRepository motoristaRepository, IMapper _mapper, MotoristaInputModel model) =>
 {
     var motoristaVM = _mapper.Map<MotoristaViewModel>(model);
     var motoristaDM = _mapper.Map<Motorista>(motoristaVM);
@@ -328,28 +372,35 @@ app.MapPost("/v1/Create", async (IMotoristaRepository motoristaRepository, IMapp
     {
         motoristaDM.Id
     });
-}).WithName("CreateMotorista").WithTags("Motorista");
+})
+    .RequireAuthorization()
+    .WithTags("Motorista");
 
-app.MapDelete("/v1/DeleteById", async (
-    IMotoristaRepository motoristaRepository,
-    Guid idMotorista) =>
+app.MapDelete("/api/Motorista/Delete", async (IMotoristaRepository motoristaRepository, Guid idMotorista) =>
 {
     await motoristaRepository.Remove(idMotorista);
 
-    return Results.Ok();
-}).WithName("DeleteMotorista").WithTags("Motorista");
+    return Results.Ok(new
+    {
+        message = "Motorista excluído com sucesso!"
+    });
+})
+    .RequireAuthorization()
+    .WithTags("Motorista");
 
-app.MapPut("/v1/Update", async (
-    IMotoristaRepository motoristaRepository,
-    IMapper _mapper,
-    MotoristaViewModel model) =>
+app.MapPut("/api/Motorista/Update", async (IMotoristaRepository motoristaRepository, IMapper _mapper, MotoristaViewModel model) =>
 {
     var motoristaDM = _mapper.Map<Motorista>(model);
 
     await motoristaRepository.Update(motoristaDM);
 
-    return Results.Ok();
-}).WithName("UpdateMotorista").WithTags("Motorista");
+    return Results.Ok(new
+    {
+        message = "Motorista atualizado com sucesso!"
+    });
+})
+    .RequireAuthorization()
+    .WithTags("Motorista");
 
 #endregion
 
